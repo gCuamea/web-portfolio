aef3f6995b0430d776274bcc63427c1e12cf6c74
Alonso Cuamea <gcuamea09@gmail.com>
2021-04-13 23:00:48 -0500

    Started Main development

--- src/PortfolioApp.js
+++ src/PortfolioApp.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Main } from './Components/Main';
 import { Navbar } from './Components/Navbar';
 
 export const PortfolioApp = () => {
   return (
     <>
       <Navbar />
+      <Main />
     </>
   );
 };
