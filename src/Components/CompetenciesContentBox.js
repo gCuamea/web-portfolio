e405cd82dfb608a0993627d6475696aed95f72d1
Alonso Cuamea <gcuamea09@gmail.com>
2021-04-17 17:14:51 -0500

    Added observer for content box

--- src/Components/CompetenciesContentBox.js
+++ src/Components/CompetenciesContentBox.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 
 export const CompetenciesContentBox = ({ title, text }) => {
+  const containerRef = useRef(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const animate = (entries) => {
+      const [entry] = entries;
+
+      if (entry.isIntersecting && !isVisible) {
+        setIsVisible(true);
+      }
+    };
+
+    const observer = new IntersectionObserver(animate, {
+      root: null,
+      rootMargin: '0px',
+      threshold: 1,
+    });
+
+    if (containerRef.current) observer.observe(containerRef.current);
+  }, [isVisible]);
+
   return (
-    <CompetenciesContentContainer>
+    <CompetenciesContentContainer
+      ref={containerRef}
+      style={{ opacity: `${isVisible ? '1' : '0'}` }}
+      className={`animate__animated ${isVisible ? 'animate__fadeIn' : ''}`}
+    >
       <h2 className="uppercase letter-spacing-sm">{title}</h2>
       <p>{text}</p>
     </CompetenciesContentContainer>
